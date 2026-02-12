343f15ea37e85bb088e921e4e856f6eca34e8d95
pavanDT10 <pkaadelli@gmail.com>
2026-02-12 22:14:41 +0530

    Fixed sqlite error for cloud deployment

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
+# --- PATCH FOR STREAMLIT CLOUD (SQLite Fix) ---
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import pandas as pd
 import sys
